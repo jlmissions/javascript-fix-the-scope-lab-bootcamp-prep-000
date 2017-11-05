55ebd1b229987721e82694f135d8126cf3067404
jlmissions@yahoo.com <jlmissions@yahoo.com>
2017-11-05 23:06:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,13 +11,11 @@ function yourAnimal() {
   // P.S.: You can't just hard-code 'cat' below
   return animal
 }
-  
+
 function add2(n) {
   const two = 2
   return parseInt(n) + two
-
   // Feel free to move things around!
-
 }
 
 var funkyFunction = function() {
@@ -28,4 +26,4 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var funkyFunction = function()
